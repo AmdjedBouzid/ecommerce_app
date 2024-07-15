a1b9f2561d0637ddb8d8905bcca519c09fe094f6
Amdjed <amdjedbouzid9@gmail.com>
2024-07-15 03:14:10 +0200

    a

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,2 +1,1 @@
-export const DOMAIN =
-  "https://ecommerce-arom4lal8-amdjedbouzids-projects.vercel.apphttps://ecommerce-arom4lal8-amdjedbouzids-projects.vercel.app";
+export const DOMAIN = "https://ecommerce-app-psi-three.vercel.app/";
